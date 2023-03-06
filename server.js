4b095d6290b24dce5d47fbadf41ba4aeddbd3e88
Mahmoud Kamal Fawzy <65209396+mahmoud947@users.noreply.github.com>
2023-03-06 20:29:20 +0200

    create models with mongoose and connect to mongo db

--- server.js
+++ server.js
@@ -1,4 +1,40 @@
 const express = require("express")
-
+const dotenv = require("dotenv")
+const { chats } = require("./data/data")
+const connectDb = require("./config/db")
 const app = express()
-app.listen(5000, console.log("Server Started on Port 5000"))
+
+dotenv.config()
+connectDb()
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.json({ message: "hello" })
+})
+
+
+app.get('/api/chat', (req, res) => {
+    res.send(chats)
+})
+
+app.get('/api/chat/:id', (req, res) => {
+    const singleChat = chats.find(c => c._id == req.params.id)
+    res.send(singleChat)
+
+})
+
+
+
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, console.log(`Server Started on Port ${PORT}`))
